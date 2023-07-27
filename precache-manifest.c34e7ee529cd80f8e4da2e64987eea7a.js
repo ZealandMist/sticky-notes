16e41e468e9708a99bcc00c9f8489620aca3f8b1
ZealandMist <jencihla@gmail.com>
2023-07-26 20:18:21 -0400

    Updates

---
+++ precache-manifest.c34e7ee529cd80f8e4da2e64987eea7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3712c78b2db7866c70a1",
+    "url": "/sticky-notes/static/css/main.5693118f.chunk.css"
+  },
+  {
+    "revision": "3712c78b2db7866c70a1",
+    "url": "/sticky-notes/static/js/main.9baf9aab.chunk.js"
+  },
+  {
+    "revision": "eb4d8215ed7fa83de4b7",
+    "url": "/sticky-notes/static/js/runtime~main.98137e24.js"
+  },
+  {
+    "revision": "aacd201bf72820a96c02",
+    "url": "/sticky-notes/static/js/2.b4876291.chunk.js"
+  },
+  {
+    "revision": "49721deff5e30d448fa4e061de9ef985",
+    "url": "/sticky-notes/index.html"
+  }
+];
